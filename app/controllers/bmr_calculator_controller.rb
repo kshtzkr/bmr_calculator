3ac659d81afaf061610c03bc8e6af2e6f04ff2e6
kshtzkr <kshtzkr@gmail.com>
2018-07-12 01:50:55 +0530

    logic changes

--- app/controllers/bmr_calculator_controller.rb
+++ app/controllers/bmr_calculator_controller.rb
@@ -8,9 +8,17 @@ class BmrCalculatorController < ApplicationController
 		@weight = params[:weight]
 		@gender = params[:gender]
 		if @gender == 'male'
-			@bmr = 66.5 + (13.75*@weight.to_i) + (5.003*@height.to_i) - (6.755*@age.to_i)
+			# original method
+			# @bmr = 66.5 + (13.75*@weight.to_i) + (5.003*@height.to_i) - (6.755*@age.to_i)
+
+			# revised method
+			@bmr = (10*@weight.to_i)+(6.25*@height.to_i)-(5*@age.to_i)+5
 		elsif @gender == 'female'
-			@bmr = 655.1 + (9.563*@weight.to_i) + (1.850*@height.to_i) - (4.676*@age.to_i)
+			# original method
+			# @bmr = 655.1 + (9.563*@weight.to_i) + (1.850*@height.to_i) - (4.676*@age.to_i)
+
+			# revised method
+			@bmr = (10*@weight.to_i)+(6.25*@height.to_i)-(5*@age.to_i)-161
 		end
 	end
 end
